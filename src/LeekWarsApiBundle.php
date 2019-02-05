4c3cf0b3d637d22cc917552c9e382e9f3ce8d56a
IceMaD <icemad1109@gmail.com>
2019-02-06 00:59:08 +0100

    Add extension to avoid bundle config requirement

--- src/LeekWarsApiBundle.php
+++ src/LeekWarsApiBundle.php
@@ -2,8 +2,13 @@
 
 namespace IceMaD\LeekWarsApiBundle;
 
+use IceMaD\LeekWarsApiBundle\DependencyInjection\ServicesExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class LeekWarsApiBundle extends Bundle
 {
+    public function getContainerExtension()
+    {
+        return new ServicesExtension();
+    }
 }
